ae4641607270febace15f2e04c6d5af71ecec0dd
John DeNisco <jadenisco17@gmail.com>
2023-10-11 15:05:01 -0400

    Fix wiki path

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 
 from . import util
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
