0994d7b5209742a0eb4570ecfe74966b62f5851c
John DeNisco <jadenisco17@gmail.com>
2023-10-17 08:11:11 -0400

    Finish search

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -21,14 +21,19 @@ def index(request):
     entries = []
 
     if 'q' in request.GET:
-        entries = _list_entries(request.GET['q'])
+        search_string = request.GET['q']
+        entries = _list_entries(search_string)
+        # Brings you directly to the entry, not exactly the
+        # assignment, but saves a step to get to the page.
+        # remove these 2 lines for the exact assignment functionality
         if len(entries) == 1:
             return(title(request, entries[0]))
     else:
         entries = _list_entries()
 
     return render(request, "encyclopedia/index.html", {
-        "entries": entries
+        "entries": entries,
+        "search_string": search_string
     })
 
 def title(request, title):
