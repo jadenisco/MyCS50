e6ee42a1f0a396c10144d08c6fb67a7e01ccb761
John DeNisco <jadenisco17@gmail.com>
2023-10-16 20:34:40 -0400

    Add search functionality

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -4,9 +4,31 @@ from markdown2 import Markdown
 
 from . import util
 
+def _list_entries(search_string=None):
+    s_entries = []
+
+    if search_string == None:
+        return util.list_entries()
+    
+    for s in util.list_entries():
+        if search_string.lower() in s.lower():
+            s_entries.append(s)
+
+    return s_entries
+
 def index(request):
+    search_string = None
+    entries = []
+
+    if 'q' in request.GET:
+        entries = _list_entries(request.GET['q'])
+        if len(entries) == 1:
+            return(title(request, entries[0]))
+    else:
+        entries = _list_entries()
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
 def title(request, title):
