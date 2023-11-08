988cf2c8bcf8ed06320dfc2431bce4f86d76d575
John DeNisco <jadenisco17@gmail.com>
2023-11-08 06:55:58 -0500

    Moved over code from materials, do models next.

--- project2/commerce/commerce/auctions/urls.py
+++ project2/commerce/commerce/auctions/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("logout", views.register, name="register")
 ]
