312ce5ec46da0458c3c4dd0a3c4de18fdf8f58e4
John DeNisco <jadenisco17@gmail.com>
2023-11-12 14:58:22 -0500

    Started models and index

--- project2/commerce/commerce/auctions/models.py
+++ project2/commerce/commerce/auctions/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"ID: {self.id}, Name: {self.first_name} {self.last_name} Email: {self.email}"
+
+class Auction(models.Model):
+    seller = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="seller")
+    time_created = models.IntegerField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"ID: {self.id}, Seller: {self.seller}"
+
+class Listing(models.Model):
+    description = models.CharField(max_length=64)
+    price = models.IntegerField()
+    created = models.IntegerField()
+    auctions = models.ManyToManyField(Auction, blank=True, related_name="listings")
+
+    def __str__(self):
+        return f"ID: {self.id}, Description: {self.description}"
+
+class Bid(models.Model):
+    price = models.IntegerField()
+
+    def __str__(self):
+        return f"ID: {self.id}, Price: {self.price}"
+class Comment(models.Model):
+    comment = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"ID: {self.id}, Comment: {self.comment}"
+
+
+'''
+class User(AbstractUser):
+    pass
+'''
