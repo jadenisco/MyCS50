b794222a06fe27fc5ce8f8b3842d1d95b8f1aec8
John DeNisco <jadenisco17@gmail.com>
2024-04-08 08:28:25 -0400

    First cut at models, next, post in views

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(blank=True)
+    likes = models.IntegerField(default=0)
+    comment = models.TextField(blank=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user,
+            "title": self.title,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p"),
+            "body": self.body,
+            "likes": self.likes
+        }
